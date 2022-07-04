d22d88509bbedc8f3b4abb6162e663584b4a98c1
ehdrb92 <kcj920407@gmail.com>
2022-07-04 13:59:14 +0900

    database revise

--- products/views.py
+++ products/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
-# Create your views here.
+import json
+
+from django.http import JsonResponse
+from django.views import View
+
+from products.models import Menu, Category, Drink
+
+class ProductsView(View):
+    def post(self, request):
+        data      = json.loads(request.body)
+        menu1     = Menu.objects.create(name=data['menu'])
+        category1 = Category.objects.create(
+            name = data['categories'],
+            menu = menu1
+        )
+        Drink.objects.create(
+            korean_name = data['drinks'], 
+            category = category1
+        )
+
+        return JsonResponse({'messasge':'created'}, status=201)
+
+class ProductsView(View):
+    def get(self, request):
+        products = Drink.objects.all()
+        results  = []
+
+        for drink in products:
+           results.append(
+               {
+                   "menu" : drink.category.menu.name,
+                   "category" : drink.category.name,
+                   "product_name" : drink.korean_name
+               }
+           )
+       
+        return JsonResponse({'resutls':results}, status=200)
